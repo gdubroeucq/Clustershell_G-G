cd333b2cfc21b78ae06d828a198c36eea608c953
gdubroeucq <dubroeucqg@gmail.com>
2016-10-07 17:05:56 +0200

    gestion de service en ligne de commande

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# utilisation:
+# ./script.py noeuds service1 commande service2 commande ...
 
 import sys
 from ClusterShell.Task import task_self, NodeSet
@@ -7,19 +9,27 @@ from ClusterShell.Task import task_self, NodeSet
 
 def main():
 
+    argument = []
+    for arg in sys.argv:
+        argument.append(arg)
 
-    task = task_self()
-    task.shell("date", nodes="node[2-3]")
-    task.run()
+    #print(len(argument))
+ 
+    for i in range(2,len(argument)):
+        if i%2 == 0:    # pour chaque chiffre pair
+            if(argument[i+1] == 'start' or 'stop' or 'status'):
+                task = task_self()
+                cli="service %s %s"% (argument[i], argument[i+1])
+                task.shell(cli, nodes=argument[1])
+                task.run() 
+ 
 
     for output, nodelist in task.iter_buffers():
+        print '\n'
         print '%s: %s' % (NodeSet.fromlist(nodelist), output)
 
+
     
-
-
-
-
 
 if __name__ == '__main__':
     main()
