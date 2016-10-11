59065e579c47c82a3845f2baca8de8366531dc30
gdubroeucq <dubroeucqg@gmail.com>
2016-10-11 18:49:50 +0200

    partie ouverture de fichier complète

--- yamlscript.py
+++ yamlscript.py
@@ -1,32 +1,50 @@
 #!/usr/bin/env python
+#coding: utf8
 
-import yaml
+import yaml,sys,os
 
 
 
 def main():
-    with open("example.yaml",'r') as stream:
-        try:
-            doc=yaml.load(stream)
-           # print(doc)
-           # print(doc.get("service")["name"])
-           # print(doc[1].get("ssh").get("state"))
-           #print(doc[1].keys())
-            print(doc[1].keys()[0])
+
+    if(len(sys.argv)<2):
+        print("/!\\ fichier.yaml en argument requis /!\\")
+    else: 
+        if(os.path.isfile(sys.argv[1])==True):   # vérifie l'existence du fichier yaml
+            fichier = sys.argv[1]
+            with open(fichier,'r') as stream:
+                try:
+                    doc=yaml.load(stream)
+                    print("------------")                              #####################
+                    print("nombre de service: %d" % len(doc))          #                   #
+                    print(type(doc))                                   # Section debugging #
+                    print(doc)                                         #                   #
+                    print("------------")                              ##################### 
+                    service=[]
+                    if(len(doc)!=0 and isinstance(doc,list)==True):
+                        for i in range(0,len(doc)):
+                            service.append(doc[i].keys()[0]) 
+                        print("D'après votre fichier \"%s\", les services concernés sont:" % sys.argv[1])
+                        compt=0
+                        for n in service:
+                            print("   %d: %s" % (compt,n))
+                            compt += 1
+                        rep=""
+                        print("\n")
+                        while(rep !='y' and rep !='n'):
+                            rep = raw_input("Confirmer (y/n) : ")
+                        if(rep=='y'):
+                            #Suite du code ici 
+                            print("ok")
+                         
+
+                    else:
+                        print("Erreur syntaxe YAML")
             
-            #for i in doc[0].keys():
-                
-           # for cle in doc.keys():
-           #     print cle
-            
-        except yaml.YAMLError as exc:
-            print(exc)
-
-
-
-
-
-
+                except yaml.YAMLError as exc:
+                    print(exc)
+        else:
+            print("\"%s\" n'existe pas" % sys.argv[1])
 
 
 
