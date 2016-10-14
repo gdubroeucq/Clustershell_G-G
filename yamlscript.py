e406bb2a04bc92beffe8020db55c83849fabb627
gdubroeucq <dubroeucqg@gmail.com>
2016-10-14 19:48:46 +0200

    changement syntaxe en seulement dictionnaire, manque la gestion des noeuds

--- yamlscript.py
+++ yamlscript.py
@@ -2,6 +2,7 @@
 #coding: utf8
 
 import yaml,sys,os
+from ClusterShell.Task import task_self, NodeSet
 
 
 
@@ -21,25 +22,52 @@ def main():
                     print(doc)                                         #                   #
                     print("------------")                              ##################### 
                     service=[]
-                    if(len(doc)!=0 and isinstance(doc,list)==True):
-                        for i in range(0,len(doc)):
-                            service.append(doc[i].keys()[0]) 
+                    if(isinstance(doc,dict)==True and len(doc.keys())!=0):
+                        print(len(doc.keys()))
                         print("D'après votre fichier \"%s\", les services concernés sont:" % sys.argv[1])
                         compt=0
-                        for n in service:
-                            print("   %d: %s" % (compt,n))
+                        service=[]
+                        for cle in reversed(doc.keys()):    #remettre les clés dans le bon ordre
+                            print("   %d: %s" % (compt,cle))
                             compt += 1
+                            service.append(cle)
                         rep=""
+
                         print("\n")
                         while(rep !='y' and rep !='n'):
                             rep = raw_input("Confirmer (y/n) : ")
                         if(rep=='y'):
-                            #Suite du code ici 
-                            print("ok")
-                         
+                            print '\n'
+                            for i in range(0,len(service)):  # vérification
+                                name=service[i]
+                                if(doc.get(name).has_key("state")==False): 
+                                    print("/!\\ attribut \"state\" manquant pour %s /!\\" % name)
+                                    sys.exit()
+                                    if not(doc.get(name).get("state") in ['start','stop','status','reload']):
+                                        print("/!\\ attribut \"state\" doit être start,stop,status ou reload /!\\") 
+                                        sys.exit()
+                                if(doc.get(name).has_key("nodes")==False or doc.get(name).get("nodes")==None):
+                                    print("/!\\ attribut \"nodes\" manquant pour %s /!\\" % name)
+                                    sys.exit()
+                                #print(" name: %s       state: %s       nodes: %s" % (name,state,nodes)) 
+                            for i in range(0,len(service)):
+                                task = task_self()
+                                name=service[i]
+                                state=doc.get(name).get("state")
+                                nodes=doc.get(name).get("nodes")
+                                cli="service %s %s in %s" % (name,state,nodes)     
+                                task.shell(cli, nodes=nodes) 
+                                task.run()
+                                print("- name: %s     state: %s         nodes: %s" % (name,state,nodes))
+                                for output, nodelist in task.iter_buffers():
+                                  #  print '\n'
+                                    print '--> %s: %s' % (NodeSet.fromlist(nodelist), output) 
+                                    print '\n'
+                            print '\n'
+                            
 
                     else:
-                        print("Erreur syntaxe YAML")
+                        print("/!\\ Erreur syntaxe dans \"%s\" /!\\" % sys.argv[1])
             
                 except yaml.YAMLError as exc:
                     print(exc)
