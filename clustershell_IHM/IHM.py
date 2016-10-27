e9153391a89bb84274af3a0e2673b249a4285b87
root <dubroeucqg@gmail.com>
2016-10-27 20:09:07 +0200

    exemple de fonctionnement

--- clustershell_IHM/IHM.py
+++ clustershell_IHM/IHM.py
@@ -4,6 +4,8 @@
 #!/usr/bin/python2.7
 
 from PyQt4 import QtGui, QtCore
+from PyQt4.QtCore import pyqtSlot
+from PyQt4.QtGui import *
 import sys
 
 import clustershell_IHM
@@ -12,6 +14,7 @@ class clustershell_IHM(QtGui.QMainWindow, clustershell_IHM.Ui_MainWindow):
     def __init__(self, parent=None):
         super(clustershell_IHM, self).__init__(parent)
         self.setupUi(self)
+        #self.label.setText("test")
 
     def main(self):
         self.show()
@@ -19,5 +22,17 @@ class clustershell_IHM(QtGui.QMainWindow, clustershell_IHM.Ui_MainWindow):
 if __name__=='__main__':
     app=QtGui.QApplication(sys.argv)
     clustershell_IHM = clustershell_IHM()
+    #clustershell_IHM.label.setText("test")
+    @pyqtSlot()
+    def on_click():
+        #print('clicked')
+        clustershell_IHM.label.setText("test")
+
+
+
+    clustershell_IHM.pushButton.clicked.connect(on_click)
     clustershell_IHM.main()
     app.exec_()
+
+    #Label=QLabel("wesh")
+    #Label.show()
